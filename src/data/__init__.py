7197eb20c53dda6beae7cb4ae88ff672260b8009
NikKaem <57253647+NikKaem@users.noreply.github.com>
2023-08-03 23:22:32 +0200

    updated init with m5 dataset

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,5 @@
 from src.data.cancer_sim import SyntheticCancerDatasetCollection
+from src.data.m5 import M5DatasetCollection
 from src.data.mimic_iii import MIMIC3RealDatasetCollection, MIMIC3SyntheticDatasetCollection
 from src.data.dataset_collection import RealDatasetCollection, SyntheticDatasetCollection
+
